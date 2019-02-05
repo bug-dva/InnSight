868505295de75d8389907539200f8fcaa8e47eee
BugDiva <liurongamy@gmail.com>
2019-02-04 21:39:38 -0800

    revised data_fetch folder,deleted test files

--- data_fetch/listname.py
+++
@@ -1,7 +0,0 @@
-file_list = []
-fh = open("filelist.txt", "r")
-for line in fh.readlines():
-    line = line.strip()
-    line_url = 's3n://airbnbdataset/allcsvfile/'+str(line)
-    file_list.append(line_url)
-print(file_list)
